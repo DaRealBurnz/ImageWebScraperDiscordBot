498364cec5343be5e47369b6ceaa13d8bb3fe17e
DaRealBurnz <87015034+DaRealBurnz@users.noreply.github.com>
2025-01-06 00:30:48 +1100

    Updated tasks system so multiple tasks can run simultaneously

--- main.py
+++ main.py
@@ -11,77 +11,98 @@ import json
 from dotenv import load_dotenv
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.default())
-html = requests.get('https://www.bigwatermelon.com.au/dailyspecials/')
-soup = BeautifulSoup(html.text, 'html.parser')
-with open('config.yml', 'r') as f:
-  config = yaml.safe_load(f)
-ignoreLst = config['ignoreList']
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
+ignoreLst = config["ignoreList"]
+html = requests.get(config["url"])
+soup = BeautifulSoup(html.text, "html.parser")
 load_dotenv()
+
 
 # Load data from guild_info file. If it doesn't exist consider it empty
 def loadGuildInfo() -> dict:
-  try:
-    with open('guild_info.json', 'r') as f:
-      return json.load(f)
-  except FileNotFoundError:
-    return {}
+    try:
+        with open("guild_info.json", "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        return {}
+
 
 # Save data to guild_info file. Will create if it does not exist
 def saveGuildInfo(guildInfo: dict):
-  with open('guild_info.json', 'w') as f:
-    json.dump(guildInfo, f)
+    with open("guild_info.json", "w") as f:
+        json.dump(guildInfo, f)
 
-@tasks.loop(hours=1)
+
+# @tasks.loop(hours=1)
 async def checkImgUpdate(guild: discord.Guild):
-  print(f'checking img update for guild {guild.id}')
-  imgs = soup.select(config['selector'])
-  guildInfo = loadGuildInfo()
-  if str(guild.id) in guildInfo.keys():
-    g = guildInfo[str(guild.id)]
-    imgLink = ""
-    for img in imgs:
-      if img['src'] not in ignoreLst:
-        imgLink = imgLink + img['src'] + "\n"
-    if imgLink and imgLink != g['last_img']:
-      chnl = guild.get_channel(g['channel'])
-      await chnl.send(imgLink)
-      g['last_img'] = imgLink
-      saveGuildInfo(guildInfo)
-  else:
-     print(f"cannot find guild info for guild {guild.id}")
+    print(f"checking img update for guild {guild.id}")
+    imgs = soup.select(config["selector"])
+    guildInfo = loadGuildInfo()
+    if str(guild.id) in guildInfo.keys():
+        g = guildInfo[str(guild.id)]
+        imgLink = ""
+        for img in imgs:
+            if img["src"] not in ignoreLst:
+                imgLink = imgLink + img["src"] + "\n"
+        if imgLink and imgLink != g["last_img"]:
+            chnl = guild.get_channel(g["channel"])
+            await chnl.send(imgLink)
+            g["last_img"] = imgLink
+            saveGuildInfo(guildInfo)
+    else:
+        print(f"cannot find guild info for guild {guild.id}")
 
-@client.tree.command(name="setchannel", description="Set this channel to receive updates from the bot")
+
+runningTasks = []
+
+
+def startTask(guild: discord.Guild):
+    # If task is already running, don't do anything
+    for task in runningTasks:
+        if task.getTask().get_name() == str(guild.id):
+            return
+        
+    # Create new task and add it to the list
+    t = tasks.loop(name=str(guild.id),hours=1)(checkImgUpdate)
+    runningTasks.append(t)
+    t.start(guild)
+
+
+@client.tree.command(
+    name="setchannel", description="Set this channel to receive updates from the bot"
+)
 async def setChannel(interaction: discord.Interaction):
-  guildInfo = loadGuildInfo()
-  if str(interaction.guild_id) not in guildInfo.keys():
-    guildInfo[str(interaction.guild_id)] = {}
-    guildInfo[str(interaction.guild_id)]['last_img'] = ""
-  guildInfo[str(interaction.guild_id)]['channel'] = interaction.channel_id
-  saveGuildInfo(guildInfo)
-  await interaction.response.send_message("This channel has been set to publish updates", silent=True)
+    guildInfo = loadGuildInfo()
+    if str(interaction.guild_id) not in guildInfo.keys():
+        guildInfo[str(interaction.guild_id)] = {"last_img": ""}
+    guildInfo[str(interaction.guild_id)]["channel"] = interaction.channel_id
+    saveGuildInfo(guildInfo)
+    await interaction.response.send_message(
+        "The bot will post updates to this channel", silent=True
+    )
+    startTask(interaction.guild)
+
+
+# @client.tree.command(name="checkupdate")
+
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
     await client.tree.sync()
-    # checkImgUpdate.start(client.get_guild(487606526098538497))
+    guildInfo = loadGuildInfo()
+    for guild in guildInfo:
+        startTask(client.get_guild(int(guild)))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    # if message.content.startswith('$force'):
-    #     await uploadImg(db["img"])
 
 try:
     client.run(os.getenv("BOT_TOKEN"))
 except discord.HTTPException as e:
     if e.status == 429:
         print("The Discord servers denied the connection for making too many requests")
-        print("Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests")
+        print(
+            "Get help from https://stackoverflow.com/questions/66724687/in-discord-py-how-to-solve-the-error-for-toomanyrequests"
+        )
     else:
         raise e
